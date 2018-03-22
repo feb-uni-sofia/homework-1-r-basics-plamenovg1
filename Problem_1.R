c2ec28e34afd511d457807d6648c25cbb6406dab
plamenovg1 <37447153+plamenovg1@users.noreply.github.com>
2018-03-22 11:19:20 +0200

    Add files via upload
    Homework 1

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,46 @@
+# HOMEWORK 1
+
+# PROBLEM 1
+
+#a)
+
+x<-c(4,1,1,4)
+
+#b)
+
+y<-c(1,4)
+
+#c) Shorter vectors in the expression are recycled as often as need be until they match the length of the longest vector.
+
+x-y
+
+#d)
+
+s<-c(x,y)
+
+#e)
+
+rep(s,10)
+srep <- rep(s,10)
+length(srep)
+
+#f)
+
+rep(s, each=3)
+
+#g)
+
+seq(7,21, by=1)
+
+7:21
+
+#h)
+
+lseq <- seq(7,21, by=1)
+length(lseq)
+
+
+
+
+
 
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,42 @@
 
+# PROBLEM 2
+
+#a)
+
+xmin <- c(23.0, 20.5, 28.2, 20.3, 22.4, 17.2, 18.2)
+xmax <- c(25.0, 22.8, 31.2, 27.3, 28.4, 20.2, 24.1)
+
+#b)
+
+dff <-  xmax - xmin
+
+#c)
+
+avgMAX <- mean(xmax)
+avgMIN <- mean(xmin)
+
+#d)
+
+xmin [xmin < avgMIN]
+
+# e)
+
+xmin [xmax > avgMAX]
+
+# f)
+
+names(xmin) <- c('Mon', 'Tue', 'Wed', 'Thu', 'Fri' , 'Sat' , 'Sun')
+names(xmax) <- c('Mon', 'Tue', 'Wed', 'Thu', 'Fri' , 'Sat' , 'Sun')
+
+# g)
+
+temperature <- data.frame(xmin,xmax)
+
+# h)
+
+temperature <- within(temperature,{xminFahrenheit <- xmin*9/5 + 32})
+
+#i)
+
+ftemp <- data.frame(Max_temp_Fahr = xmax*9/5 + 32 , Min_temp_Fahr = xmin*9/5 + 32)
+
